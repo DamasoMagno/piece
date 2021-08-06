940409b9ecfca9f2b48f1d65fcdbe0b2627c7bd5
DamasoMagno <limamdamaso@gmail.com>
2021-08-06 08:27:24 -0300

    HTMLComponents fucional

--- Components/index.js
+++ Components/index.js
@@ -1,5 +1,5 @@
 const body = document.querySelector("body");
-const bodyChildren = document.querySelectorAll("body *");
+const bodyChildren = body.querySelectorAll("*");
 const customTagsFiltered = [...bodyChildren].filter(tag => customTagsFilter(tag));
 const tagsFormatted = customTagsFiltered.map( item => formattTags(item));
 
@@ -11,75 +11,57 @@ function formattTags(item){
   return item.tagName.split("-")[0];
 }
 
+const Errors = {
+  generateHTMLError(error){  
+    const errorHTML = `
+      <div style="width: 80%; max-width: 500px; min-height: 200px; background-color: rgb(196, 0, 0); position: fixed;
+      top: 50%; left: 50%; right: 0px; transform: translate(-50%, -50%); border-radius: 6px; padding: 4px;
+      box-sizing: border-box; box-shadow: 0px 0px 6px #00000079; border: 2px solid #ff9900;
+      letter-spacing: 0.25px;">
+        <h4 style="width: 100%; color: #ffd700; border-bottom: 1.4px solid #ff9900; padding-bottom: .3rem; 
+        margin-top: .4rem; user-select: none; font-size: 18px;">
+          Error: ${error.status} 
+        </h4>
+        <p style="color: #ffd700; font-size: 16px; word-break: break-all;  
+        letter-spacing: .1rem; line-height: 1.25rem;">
+          File: ${error.url} </br> ${error.statusText.toLowerCase()}
+        </p>
+      </div>  
+    `;
+
+    return errorHTML;
+  }
+};
+
 async function getAllComponents(){
   const response = await fetch("./Components/Components.json");
   const allComponents = await response.json();
   return allComponents;
 }
 
-function verifyIfComponentExist(Component){
-  const component = Component.name.toUpperCase();
-  return tagsFormatted.some(tag => component === tag);
-}
-
-function removeDeadTags(component = false){
-  if(!!component.length){
-    customTagsFiltered.forEach(tag => {
-      if(tag.tagName.split("-")[0] !== component.toUpperCase()){
-        body.removeChild(tag);
-      }
-    });
-
-    return;
-  }  
-
-  customTagsFiltered.forEach(tag => {
-    body.removeChild(tag);
-  });
-} 
-
-getAllComponents()
-.then( Components => {
-  if(!!Components.length){
-    for (const Component of Components) {
-      const componentsExists = verifyIfComponentExist(Component) && Component.name;
-      if (!componentsExists) {
-        continue;
-      }
-      renderComponent(componentsExists);
-    }
-    return;
-  }
-  removeDeadTags();
-});
-
-
 function createCustomTag(className, contentHTML){
-  const classes = {};
-  classes[className] = class extends HTMLElement {
+  class customTag extends HTMLElement {
     constructor(){
       super();
     }
 
-    formatSelector(selector){
-      return selector === "id" ? "#" : ".";
+    formatSelector(key, nameSelectorPosition){
+      let typeSelector = "";
+      const selectorName = key.slice(0, nameSelectorPosition);
+      if(selectorName === "id"){
+        typeSelector = "#";
+      } else if(selectorName === "class"){
+        typeSelector = ".";
+      }
+
+      return typeSelector;
     }
 
     createSelector(key, nameSelectorPosition, typeAttributePosition){
-      const typeSelector = this.formatSelector(key.slice(0, nameSelectorPosition));
+      const typeSelector = this.formatSelector(key, nameSelectorPosition);
       const nameSelector = key.slice(nameSelectorPosition, typeAttributePosition).toLowerCase();
-
       const selector = typeSelector.concat(nameSelector);
       return selector;
-    }
-
-    generateStyle(){
-      const head = document.querySelector("head");
-      const link = document.createElement("link");
-
-      link.href = `../../Components/${className}/style.css`;
-      link.rel = "stylesheet";
-      head.appendChild(link);
     }
 
     setContentOfAttributes(data){
@@ -90,10 +72,10 @@ function createCustomTag(className, contentHTML){
         return;
       }
 
-
       if(type === "attributes" && element){
         content.split(",").forEach( attribute => {
           const [attributeType, attributeValue] = attribute.split("-");
+
           if(!!element.classList.length && attributeType.trim("") === "class"){
             element.classList.add(attributeValue);
             return;
@@ -108,35 +90,45 @@ function createCustomTag(className, contentHTML){
     setOwnAttributes(){
       Object.keys(this.dataset)
       .forEach( key => {
-        let typeAttributePosition = "";
-        let nameSelectorPosition = "";
+        let typeAttributePosition = 0;
+        let nameSelectorPosition = 0;
         
         for(const letter of key){
           if(letter === letter.toUpperCase()){
             if(nameSelectorPosition){
-              typeAttributePosition = key.indexOf(letter);
-              continue; //return
+              typeAttributePosition = key.lastIndexOf(letter);
+              continue;
             }
-
+            
             nameSelectorPosition = key.indexOf(letter);
           }
         }
                 
         const type = key.slice(typeAttributePosition).toLowerCase();
         const content = this.dataset[key];
-
+        
         const selector = this.createSelector(key, nameSelectorPosition, typeAttributePosition);
         const element = document.querySelector(`${selector}`);
-
-        const data = { type, content, element }
-        this.setContentOfAttributes(data);
+        
+        this.setContentOfAttributes({ type, content, element });
       });
     }
 
-    renderComponent(){      
+    generateStyle(){
+      const head = document.querySelector("head");
+      const link = document.createElement("link");
+
+      link.href = `./Components/${className}/style.css`;
+      link.rel = "stylesheet";
+      head.appendChild(link);
+    }
+
+    renderComponent(){ 
+      if(!contentHTML) return;
+
       const domParser = new DOMParser();
-      const responseToHTML = domParser.parseFromString(contentHTML, "text/html");
-      this.replaceWith(responseToHTML.body.children[0]);
+      const convertContentToHTML = domParser.parseFromString(contentHTML, "text/html");
+      this.replaceWith(convertContentToHTML.body.children[0]);
     }
 
     connectedCallback() {
@@ -146,17 +138,52 @@ function createCustomTag(className, contentHTML){
     }
   }
   
-  let nameOfComponent = `${className.toLowerCase()}-component`;
-  customElements.define(nameOfComponent, classes[className]);
+  const nameOfComponent = `${className.toLowerCase()}-component`;
+  customElements.define(nameOfComponent, customTag);
 }
+
+function removeDeadTags(){
+  customTagsFiltered.forEach( customTag => {
+    bodyChildren.forEach( tagExisting => {
+      if(tagExisting.tagName.includes("-") && tagExisting === customTag){
+        body.removeChild(tagExisting);
+      }
+    });
+  });
+} 
+
+function verifyIfComponentExists(Component){
+  const component = Component.component.toUpperCase();
+  return tagsFormatted.some(tag => component === tag);
+}
+
+getAllComponents()
+.then( Components => {
+  for(const Component of Components) {
+    const componentsExists = verifyIfComponentExists(Component) && Component.component; 
+    if(!componentsExists) continue;     
+
+    for(const customTag of customTagsFiltered){
+      if(customTag.tagName.split("-")[0] === componentsExists.toUpperCase()){
+        const indexOfCustomTag = customTagsFiltered.findIndex(tag => tag === customTag);
+        customTagsFiltered.splice(indexOfCustomTag, 1);
+        continue;
+      }
+    }
+
+    renderComponent(componentsExists);
+  }
+  removeDeadTags();
+}); 
 
 function renderComponent(component){
   fetch(`./Components/${component}/index.html`)
   .then(response => {
     if(response.status === 404){
-      return "";
+      body.innerHTML = Errors.generateHTMLError(response);
     }
 
     return response.text()
-  }).then(response => createCustomTag(component, response));
-}
+  })
+  .then(response => createCustomTag(component, response));
+};
---
+++ README.md
@@ -0,0 +1,40 @@
+# O projeto
+<p align="justify">
+ <strong>HTMLComponents</strong> é um projeto desenvolvido para atuar como um  framework simples na criação de componentes como é feito em React(outro framework Javascript), para facilitar o desenvolvimento de projetos usando <strong>HTML, CSS e Javascript Vanilla.</strong>
+</p>
+
+</br>
+
+## Estrutura de um projeto usando HTMLComponents
+```javascript
+Components
+-> ComponentName
+   -> index.html
+   -> style.css
+-> Components.json
+    [ 
+      { "name": "ComponentName" }, 
+      { "name": "ComponentName" } 
+    ]
+-> index.js
+
+index.html
+script.js
+style.css
+```
+
+</br>
+
+## Função de cada arquivo
+* Components: Conterá todos os components
+* Components.json: Irá deixar publico quais components existem na pasta Components
+* ComponentName: É a pasta do componente
+* Index.js: Arquivo responsavel por converter os conteudos de um componente em uma tag
+
+</br>
+
+## Como usar no HTML
+<p>
+ Caso o usuario queira usar algum componente, ele nescessitará realizar os seguintes passos
+</p>
+
